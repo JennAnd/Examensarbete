66045b6508fb0f32b0af387e5d97f2fff55a6fa6
s0wie <sowieh@gmail.com>
2022-11-10 16:22:45 +0100

    dashboard frontend

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,54 +4,32 @@
     <link rel="stylesheet" href="../stylesheets/dashboard.css">
 </head>
 
-<div class="side-nav">
-    <ul>
-        <li><a href="/dashboard">Översikt</a></li>
-        <li><a href="/profile">My profile</a></li>
-        <li><a href="/payments">Payments</a></li>
-    </ul>
-</div>
-{{date('Y-m-d')}}
-{{date('h:i:s')}}
-{{time()}}
-{{strtotime('now')}}
-
-@if(session()->has('message'))
-<div class="alert alert-success">
-    {{ session()->get('message') }}
-</div>
-@endif
-
-<p>Hello, {{ $user->firstname . " " . $user->lastname}}!</p>
-<p>Do you want to <a href="logout">logout?</a></p>
-
-<div class="overview">
-    <div class="booked-yogaclasses">
-        <h2>Your booked yogaclasses</h2>
-        @foreach ($bookedYogaclasses as $yogaclass)
-
-        <div>
-            <p>{{$yogaclass->class_name}}</p>
-            <p>{{$yogaclass->length}} min</p>
-            <p>{{$yogaclass->teacher}}</p>
-            <p>{{$yogaclass->date}}</p>
-            <p>yogaclass id: {{$yogaclass->id}}</p>
-            <p>{{ $cleantime = substr($yogaclass->time,0,-3)}}</p>
-            <p> Available: {{$yogaclass->available}}</p>
-            <form method="POST" action="cancelbooked">
-                @csrf
-                <input type="hidden" value="<?= $yogaclass->id ?>" name="id" id="id">
-                <button type="submit" onclick="return confirm('Do you want to cancel this yoga class?')">Cancel</button>
-            </form>
-
-            @endforeach
-        </div>
+<div class="dashboard-body">
+    <div class="side-nav">
+        <ul>
+            <li><a href="/dashboard">Översikt</a></li>
+            <li><a href="/profile">My profile</a></li>
+            <li><a href="/payments">Payments</a></li>
+        </ul>
     </div>
-    <div>
-        <div class="yogaclasses">
-            <h2>Yogaclasses</h2>
+    {{date('Y-m-d')}}
+    {{date('h:i:s')}}
+    {{time()}}
+    {{strtotime('now')}}
 
-            @foreach ($notBookedYogaclasses as $yogaclass)
+    @if(session()->has('message'))
+    <div class="alert alert-success">
+        {{ session()->get('message') }}
+    </div>
+    @endif
+
+    <p>Hello, {{ $user->firstname . " " . $user->lastname}}!</p>
+    <p>Do you want to <a href="logout">logout?</a></p>
+
+    <div class="overview">
+        <div class="booked-yogaclasses">
+            <h2>Your booked yogaclasses</h2>
+            @foreach ($bookedYogaclasses as $yogaclass)
 
             <div>
                 <p>{{$yogaclass->class_name}}</p>
@@ -60,22 +38,46 @@
                 <p>{{$yogaclass->date}}</p>
                 <p>yogaclass id: {{$yogaclass->id}}</p>
                 <p>{{ $cleantime = substr($yogaclass->time,0,-3)}}</p>
-                <p>{{strtotime($yogaclass->time)}}</p>
-                <p>{{strtotime($yogaclass->date)}}</p>
                 <p> Available: {{$yogaclass->available}}</p>
-
-                <form method="POST" action="book">
+                <form method="POST" action="cancelbooked">
                     @csrf
-                    <input type="hidden" value="<?= $yogaclass->id ?>" name="id">
-                    @if ($user->total_classes > 0 && $yogaclass->available > 0)
-                    <button type="submit" onclick="return confirm('Do you want to book?')">Book</button>
-                    @else
-                    <button disabled>Book</button>
-                    @endif
+                    <input type="hidden" value="<?= $yogaclass->id ?>" name="id" id="id">
+                    <button type="submit" onclick="return confirm('Do you want to cancel this yoga class?')">Cancel</button>
                 </form>
-            </div>
 
-            @endforeach
+                @endforeach
+            </div>
+        </div>
+        <div>
+            <div class="yogaclasses">
+                <h2>Yogaclasses</h2>
+
+                @foreach ($notBookedYogaclasses as $yogaclass)
+
+                <div>
+                    <p>{{$yogaclass->class_name}}</p>
+                    <p>{{$yogaclass->length}} min</p>
+                    <p>{{$yogaclass->teacher}}</p>
+                    <p>{{$yogaclass->date}}</p>
+                    <p>yogaclass id: {{$yogaclass->id}}</p>
+                    <p>{{ $cleantime = substr($yogaclass->time,0,-3)}}</p>
+                    <p>{{strtotime($yogaclass->time)}}</p>
+                    <p>{{strtotime($yogaclass->date)}}</p>
+                    <p> Available: {{$yogaclass->available}}</p>
+
+                    <form method="POST" action="book">
+                        @csrf
+                        <input type="hidden" value="<?= $yogaclass->id ?>" name="id">
+                        @if ($user->total_classes > 0 && $yogaclass->available > 0)
+                        <button type="submit" onclick="return confirm('Do you want to book?')">Book</button>
+                        @else
+                        <button disabled>Book</button>
+                        @endif
+                    </form>
+                </div>
+
+                @endforeach
+            </div>
         </div>
     </div>
 </div>
