4fb8fdd0daf38aaca8dae8c6d644692713b8e4c0
s0wie <sowieh@gmail.com>
2022-11-10 16:44:22 +0100

    Admin panel frontend changes

--- resources/views/adminpanel.blade.php
+++ resources/views/adminpanel.blade.php
@@ -1,108 +1,77 @@
-@include('profilenavbar')
 <link rel="stylesheet" href="../stylesheets/adminpanel.css">
 
 <body>
-    <h1>Adminpanel</h1>
+    @include('profilenavbar')
 
-    <div class="side-nav">
-        <ul>
-            <li><a href="/adminpanel">Översikt</a></li>
-            <li><a href="/invoices">Invoices</a></li>
-        </ul>
+    <div class="adminpanel-body">
+
+        <div class="side-nav">
+            <ul>
+                <li><a href="/adminpanel">Översikt</a></li>
+                <li><a href="/admin-memberships">Memberships</a></li>
+                <li><a href="/invoices">Invoices</a></li>
+                <li><a href="logout">Logout</a></li>
+            </ul>
+        </div>
+
+        <div class="make-yogaclass">
+            @if(session()->has('message'))
+            <div class="alert alert-success">
+                {{ session()->get('message') }}
+            </div>
+            @endif
+            <form method="post" action="make-yogaclass">
+                @csrf
+                <div>
+                    <label for="class_name">Class name</label>
+                    <input name="class_name" id="class_name" type="text" required />
+                </div>
+                <div>
+                    <label for="teacher">Teacher</label>
+                    <input name="teacher" id="teacher" type="text" required />
+                </div>
+                <div>
+                    <label for="date">Date</label>
+                    <input name="date" id="date" type="date" required />
+                </div>
+                <div>
+                    <label for="time">Time</label>
+                    <input name="time" id="time" type="time" required />
+                </div>
+                <div>
+                    <label for="class-length">Length</label>
+                    <input name="class-length" id="class-length" type="number" required />
+                </div>
+                <div>
+                    <label for="available">Available spots</label>
+                    <input name="available" id="available" type="number" required />
+                </div>
+                <div>
+                    <label for="reserved">Reserved</label>
+                    <input name="reserved" id="reserved" type="number" required />
+                </div>
+                <button type="submit">Add class</button>
+            </form>
+
+        </div>
+        <div class="show-yogaclasses">
+            @foreach ($yogaclasses as $yogaclass)
+            <div>
+                <p>{{$yogaclass->class_name}}</p>
+                <p>{{$yogaclass->length}} min</p>
+                <p>{{$yogaclass->teacher}}</p>
+                <p>{{$yogaclass->date}}</p>
+                <p>{{ $cleantime = substr($yogaclass->time,0,-3)}}</p>
+                <p> Available: {{$yogaclass->available}}</p>
+                <p>Reserved: {{$yogaclass->reserved}}</p>
+            </div>
+            <form action="delete-yogaclass" method="POST">
+                @csrf
+                <input type="hidden" value="<?= $yogaclass->id ?>" name="yogaclass_id">
+                <button>Delete yogaclass</button>
+            </form>
+            @endforeach
+        </div>
+
     </div>
-
-    <p>Do you want to <a href="logout">logout?</a></p>
-
-    @if(session()->has('message'))
-    <div class="alert alert-success">
-        {{ session()->get('message') }}
-    </div>
-    @endif
-
-    </nav>
-    <form method="post" action="make-yogaclass">
-        @csrf
-        <div>
-            <label for="class_name">Class name</label>
-            <input name="class_name" id="class_name" type="text" required />
-        </div>
-        <div>
-            <label for="teacher">Teacher</label>
-            <input name="teacher" id="teacher" type="text" required />
-        </div>
-        <div>
-            <label for="date">Date</label>
-            <input name="date" id="date" type="date" required />
-        </div>
-        <div>
-            <label for="time">Time</label>
-            <input name="time" id="time" type="time" required />
-        </div>
-        <div>
-            <label for="class-length">Length</label>
-            <input name="class-length" id="class-length" type="number" required />
-        </div>
-        <div>
-            <label for="available">Available spots</label>
-            <input name="available" id="available" type="number" required />
-        </div>
-        <div>
-            <label for="reserved">Reserved</label>
-            <input name="reserved" id="reserved" type="number" required />
-        </div>
-        <button type="submit">Add class</button>
-    </form>
-    @foreach ($yogaclasses as $yogaclass)
-
-    <div>
-        <p>{{$yogaclass->class_name}}</p>
-        <p>{{$yogaclass->length}} min</p>
-        <p>{{$yogaclass->teacher}}</p>
-        <p>{{$yogaclass->date}}</p>
-        <p>{{ $cleantime = substr($yogaclass->time,0,-3)}}</p>
-        <p> Available: {{$yogaclass->available}}</p>
-        <p>Reserved: {{$yogaclass->reserved}}</p>
-    </div>
-    <form action="delete-yogaclass" method="POST">
-        @csrf
-        <input type="hidden" value="<?= $yogaclass->id ?>" name="yogaclass_id">
-        <button>Delete yogaclass</button>
-    </form>
-    @endforeach
-
-    <h1>Memberships</h1>
-    <h2>Här visas alla memberships</h2>
-
-    @foreach ($memberships as $membership)
-    <div>
-        <p>{{$membership->type}}</p>
-        <p>{{$membership->price}} USD</p>
-    </div>
-    <form action="delete-membership" method="POST">
-        @csrf
-        <input type="hidden" value="<?= $membership->id ?>" name="membership_id">
-        <button>Delete membership</button>
-    </form>
-    @endforeach
-
-    <h2>Här kan man skapa memberships</h2>
-    <form action="make-membership" method="post">
-        @csrf
-        <div>
-            <label for="type">Membership type</label>
-            <input name="type" id="type" type="text" required />
-        </div>
-        <div>
-            <label for="price">Price</label>
-            <input name="price" id="price" type="text" required />
-            <p>USD</p>
-        </div>
-        <div>
-            <label for="amount_classes">Amount classes</label>
-            <input name="amount_classes" id="amount_classes" type="text" required />
-        </div>
-        <button type="submit">Add membership</button>
-    </form>
 </body>
-
-</html>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,18 +8,12 @@
     <title>Document</title>
     <link rel="stylesheet" href="../stylesheets/navbar.css">
     <link rel="stylesheet" href="../stylesheets/layout.css">
-    <link rel="stylesheet" href="../stylesheets/admin.css">
-    <link rel="stylesheet" href="../stylesheets/adminpanel.css">
-    <link rel="stylesheet" href="../stylesheets/dashboard.css">
     <link rel="stylesheet" href="../stylesheets/events.css">
     <link rel="stylesheet" href="../stylesheets/index.css">
     <link rel="stylesheet" href="../stylesheets/login.css">
     <link rel="stylesheet" href="../stylesheets/memberships.css">
     <link rel="stylesheet" href="../stylesheets/ourclasses.css">
     <link rel="stylesheet" href="../stylesheets/ourproducts.css">
-    <link rel="stylesheet" href="../stylesheets/payments.css">
-    <link rel="stylesheet" href="../stylesheets/profile.css">
-    <link rel="stylesheet" href="../stylesheets/signup.css">
     <link rel="stylesheet" href="../stylesheets/aboutus.css">
     <link rel="stylesheet" href="../stylesheets/mandala-decor.css">
 
