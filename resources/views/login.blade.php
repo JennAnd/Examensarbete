e1b0cef1f00a137393ba48026bc26b3d89eb2af4
s0wie <sowieh@gmail.com>
2022-11-11 12:36:45 +0100

    signup page frontend done

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,27 +4,29 @@
     <div class="login-container">
         <img class="login-image" src="/assets/loginpage.webp" />
         <div class="user-login">
-            <h2><a href="/">LOGO</a></h2>
-            <h1> Log in </h1>
-            @if(session()->has('message'))
-            <div class="alert alert-success">
-                {{ session()->get('message') }}
+            <h2 class="logo"><a href="/">LOGO</a></h2>
+            <div class="login-box">
+                <h1> Log in </h1>
+                @if(session()->has('message'))
+                <div class="alert alert-success">
+                    {{ session()->get('message') }}
+                </div>
+                @endif
+                @include('errors')
+                <form method="post" action="login">
+                    @csrf
+                    <div class="input-column">
+                        <label for="email">Email</label>
+                        <input name="email" id="email" type="email" required />
+                    </div>
+                    <div class="input-column">
+                        <label for="password">Password</label>
+                        <input name="password" id="password" type="password" required />
+                    </div>
+                    <button class="login-button" type="submit">Login</button>
+                    <p>Not a member? <a href="/signup">Sign up</a></p>
+                </form>
             </div>
-            @endif
-            @include('errors')
-            <form method="post" action="login">
-                @csrf
-                <div class="input-column">
-                    <label for="email">Email</label>
-                    <input name="email" id="email" type="email" required />
-                </div>
-                <div class="input-column">
-                    <label for="password">Password</label>
-                    <input name="password" id="password" type="password" required />
-                </div>
-                <button class="login-button" type="submit">Login</button>
-                <p>Not a member? <a href="/signup">Sign up</a></p>
-            </form>
         </div>
     </div>
 </body>
--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,50 +1,63 @@
+<link rel="stylesheet" href="/stylesheets/signup.css">
+
+
 <body>
-    <h1> Sign up </h1>
+    <div class="signup-container">
+        <img class="login-image" src="/assets/loginpage.webp" />
+        <div class="user-signup">
+            <h2 class="logo"><a href="/">LOGO</a></h2>
+            <div class="signup-box">
+                <h1> Sign up </h1>
+                @include('errors')
+                <form method="post" action="signup">
+                    @csrf
+                    <div>
+                        <div class="input-column">
+                            <label for="firstname">First name</label>
+                            <input name="firstname" id="firstname" type="text" required />
+                        </div>
+                        <div class="input-column">
+                            <label for="lastname">Last name</label>
+                            <input name="lastname" id="lastname" type="text" required />
+                        </div>
+                        <div class="input-column">
+                            <label for="email">Email</label>
+                            <input name="email" id="email" type="email" required />
+                        </div>
+                        <div class="input-column">
+                            <label for="password">Password (8 characters minimum)</label>
+                            <input name="password" id="password" type="password" minlength="8" required />
+                        </div>
+                        <div class="input-column">
+                            <label for="password_confirmation">Confirm password</label>
+                            <input name="password_confirmation" id="password_confirmation" type="password" required />
+                        </div>
+                    </div>
+                    <div>
+                        <div class="input-column">
+                            <label for="address">Address</label>
+                            <input name="address" id="address" type="text" required />
+                        </div>
+                        <div class="input-column">
+                            <label for="postal_code">Postal Code</label>
+                            <input name="postal_code" id="postal_code" type="number" required />
+                        </div>
+                        <div class="input-column">
+                            <label for="city">City</label>
+                            <input name="city" id="city" type="text" required />
+                        </div>
+                        <div class="input-column">
+                            <label for="country">Country</label>
+                            <input name="country" id="country" type="text" required />
+                        </div>
 
-    @include('errors')
-    <form method="post" action="signup">
-        @csrf
-        <div>
-            <label for="firstname">First name</label>
-            <input name="firstname" id="firstname" type="text" required />
+                        <button class="signup-button" type="submit">Sign up</button>
+                        <p>Already a member? <a href="/login">Log in</a></p>
+                    </div>
+                </form>
+            </div>
         </div>
-        <div>
-            <label for="lastname">Last name</label>
-            <input name="lastname" id="lastname" type="text" required />
-        </div>
-        <div>
-            <label for="email">Email</label>
-            <input name="email" id="email" type="email" required />
-        </div>
-        <div>
-            <label for="password">Password (8 characters minimum)</label>
-            <input name="password" id="password" type="password" minlength="8" required />
-        </div>
-        <div>
-            <label for="password_confirmation">Confirm password</label>
-            <input name="password_confirmation" id="password_confirmation" type="password" required />
-        </div>
-        <div>
-            <label for="address">Address</label>
-            <input name="address" id="address" type="text" required />
-        </div>
-        <div>
-            <label for="postal_code">Postal Code</label>
-            <input name="postal_code" id="postal_code" type="number" required />
-        </div>
-        <div>
-            <label for="city">City</label>
-            <input name="city" id="city" type="text" required />
-        </div>
-        <div>
-            <label for="country">Country</label>
-            <input name="country" id="country" type="text" required />
-        </div>
-
-        <button type="submit">Sign up</button>
-    </form>
-
-
+    </div>
 </body>
 
 </html>
