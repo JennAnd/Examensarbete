acdf9840afb3379c51ea070a77ae8dbb13ea9b41
JennAnd <89971271+JennAnd@users.noreply.github.com>
2022-11-13 21:57:50 +0100

    about us page

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -2,6 +2,10 @@
 @section('content')
 
 <img class="about-us-image" src="/assets/about-us-pose.webp" />
+<div class="about-us-heading-box">
+    <h1 class="about-us-heading">About Yoga name</h1>
+    <p class="about-us-heading-info">Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ame Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amett Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</p>
+</div>
 <div class="about-us-container">
     @include('mandala')
 
@@ -9,10 +13,13 @@
     <div class="about-us-boxes">
         <div class="about-us-single-box-round">
         </div>
+
         <div class="about-us-single-box">
-            <p>{{$profile->aboutUsInfo}}</p>
+            <p class="about-us-info">{{$profile->aboutUsInfo}}</p>
         </div>
+
     </div>
+
     @endforeach
 
     @include('mandala')
