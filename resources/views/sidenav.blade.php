73e38c84eadb401a89fdc34697c2f33a3ba28072
JennAnd <89971271+JennAnd@users.noreply.github.com>
2022-11-14 12:35:13 +0100

    details

--- resources/views/sidenav.blade.php
+++ resources/views/sidenav.blade.php
@@ -1,10 +1,9 @@
 <link rel="stylesheet" href="../stylesheets/sidenav.css">
 <div class="side-nav">
     <ul>
-        <li><a href="/dashboard">Översikt</a></li>
+        <li><a href="/dashboard">Overview</a></li>
         <li><a href="/profile">My profile</a></li>
         <li><a href="/payments">Payments</a></li>
-        <li><a href="/messages">Messages</a></li>
         <li><a href="/logout">Logout</a></li>
     </ul>
 </div>
