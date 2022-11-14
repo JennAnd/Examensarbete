37a9ca2ceced3c04f5a43747e145832249e5ffe2
JennAnd <89971271+JennAnd@users.noreply.github.com>
2022-11-14 15:32:49 +0100

    memberships fix

--- resources/views/memberships.blade.php
+++ resources/views/memberships.blade.php
@@ -1,20 +1,20 @@
 @extends('layout')
 @section('content')
 
-
-
 <img class="memberships-image" src="/assets/memberships.webp" />
 <h1 class="memberships-heading">Our memberships</h1>
 @include('mandala')
 
-@foreach ($memberships as $membership)
-<div class="memberships-container">
-    <p class="membership-type">{{$membership->type}}</p>
-    <p class="membership-price">{{$membership->price}} USD</p>
-    <p class="membership-validation">Valid 6 months from date of purchase</p>
-</div>
-@endforeach
 
+<div class="memberships-body">
+    @foreach ($memberships as $membership)
+    <div class="memberships-container">
+        <p class="membership-type">{{$membership->type}}</p>
+        <p class="membership-price">{{$membership->price}} USD</p>
+        <p class="membership-validation">Valid 6 months from date of purchase</p>
+    </div>
+    @endforeach
+</div>
 <p class="membership-end-section">Are you interrested in any of these memberships? <a href="/signup">Sign up</a> or <a href="/login">log in </a>to buy.</p>
 @include('mandala')
 
