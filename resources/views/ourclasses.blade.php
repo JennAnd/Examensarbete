b7d956793c1c3d671951d7189d723b625a37a616
s0wie <sowieh@gmail.com>
2022-11-14 16:02:20 +0100

    our classes grid problems solved

--- resources/views/ourclasses.blade.php
+++ resources/views/ourclasses.blade.php
@@ -7,15 +7,21 @@
 
 @include('mandala')
 
-@foreach ($classes as $class)
-<div class="yogaclasses-container">
-    <div class="yogaclasses-container-without-lotus">
-        <h3 class="yogaclass-type">{{ $class->classtype }}</h3>
-        <p class="yogaclass-info">{{$class->classinfo}}</p>
+<div class="yogaclasses-body">
+    @foreach ($classes as $class)
+    <div class="yogaclasses-container">
+        <div class="yogaclasses-container-without-lotus">
+            <h3 class="yogaclass-type">{{ $class->classtype }}</h3>
+            <p class="yogaclass-info">{{$class->classinfo}}</p>
+        </div>
+        <hr class="our-classes-hr">
     </div>
-    @include('lotus')
+
+    @endforeach
+    <!-- <div class="grid-items"></div>
+    <div class="grid-items"></div> -->
+
 </div>
-@endforeach
 <p class="our-classes-end-section">If you want to try out our yoga classes, please <a href="/signup">sign up</a> or <a href="/login">log in. </a></p>
 
 @include('mandala')
