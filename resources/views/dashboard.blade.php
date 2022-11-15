f04f4445b0e2620b023b066f973851affb3fb2a1
JennAnd <89971271+JennAnd@users.noreply.github.com>
2022-11-15 17:17:53 +0100

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
 <div class="dashboard-body">
     <div class="grid-container">
         <div class="item1">
-            <p>Hello, {{ $user->firstname . " " . $user->lastname}}!</p>
+
             <p> @if(session()->has('message'))
             <div class="alert alert-success">
                 {{ session()->get('message') }}
@@ -16,23 +16,27 @@
         <div class="item2">
             @include('sidenav')
         </div>
-
+        <div class="filler-item"></div>
         <div class="item3">
             <!-- Booked yoga classes -->
-            <h2>Your booked yogaclasses</h2>
+            <h2 class="yogaclasses-heading">Your booked yogaclasses</h2>
             @foreach ($bookedYogaclasses as $yogaclass)
+            <p class="yogaclass-date">{{date("D j/m", strtotime($yogaclass->date));}}</p>
             <div class="yogaclass">
-                <p>{{$yogaclass->class_name}}</p>
-                <p>{{$yogaclass->length}} min</p>
-                <p>{{$yogaclass->teacher}}</p>
-                <p>{{$yogaclass->date}}</p>
-                <p>yogaclass id: {{$yogaclass->id}}</p>
-                <p>{{ $cleantime = substr($yogaclass->time,0,-3)}}</p>
-                <p> Available: {{$yogaclass->available}}</p>
+                <div class="yogaclass-details">
+                    <p class="yogaclass-time">{{ $cleantime = substr($yogaclass->time,0,-3)}}</p>
+                    <p class="yogaclass-title">{{$yogaclass->class_name}}, {{$yogaclass->length}} min</p>
+                    <p>{{$yogaclass->teacher}}</p>
+
+                </div>
+                <div>
+                    <p>{{$yogaclass->reserved}}</p>
+                    <p>reserved</p>
+                </div>
                 <form method="POST" action="cancelbooked">
                     @csrf
                     <input type="hidden" value="<?= $yogaclass->id ?>" name="id" id="id">
-                    <button class="button-booked" type="submit" onclick="return confirm('Do you want to cancel this yoga class?')">Cancel</button>
+                    <button class="button-booked" type="submit" onclick="return confirm('Do you want to cancel this yoga class?')">-</button>
                 </form>
             </div>
             @endforeach
@@ -41,26 +45,30 @@
 
         <div class="item4">
             <!-- Not booked yoga classes -->
-            <h2>Yogaclasses</h2>
+            <h2 class="yogaclasses-heading">Yogaclasses</h2>
             @foreach ($notBookedYogaclasses as $yogaclass)
-            <div>
-                <p>{{$yogaclass->class_name}}</p>
-                <p>{{$yogaclass->length}} min</p>
-                <p>{{$yogaclass->teacher}}</p>
-                <p>{{$yogaclass->date}}</p>
-                <p>yogaclass id: {{$yogaclass->id}}</p>
-                <p>{{ $cleantime = substr($yogaclass->time,0,-3)}}</p>
-                <p>{{strtotime($yogaclass->time)}}</p>
-                <p>{{strtotime($yogaclass->date)}}</p>
-                <p> Available: {{$yogaclass->available}}</p>
+            <p class="yogaclass-date">{{date("D j/m", strtotime($yogaclass->date));}}</p>
+            <div class="yogaclass">
+                <div class="yogaclass-details">
+                    <p class="yogaclass-time">{{ $cleantime = substr($yogaclass->time,0,-3)}}</p>
+                    <p class="yogaclass-title">{{$yogaclass->class_name}}, {{$yogaclass->length}} min</p>
+
+                    <p>{{$yogaclass->teacher}}</p>
+
+                </div>
+                <div>
+                    <p>{{$yogaclass->available}}</p>
+                    <p>available</p>
+                </div>
+
 
                 <form method="POST" action="book">
                     @csrf
                     <input type="hidden" value="<?= $yogaclass->id ?>" name="id" id="id">
                     @if ($user->total_classes > 0 && $yogaclass->available > 0)
-                    <button type="submit" onclick="return confirm('Do you want to book?')">Book</button>
+                    <button class="button-booked" type="submit" onclick="return confirm('Do you want to book?')">+</button>
                     @else
-                    <button disabled>Book</button>
+                    <button disabled class="button-booked-disabled">+</button>
                     @endif
                 </form>
                 @endforeach
