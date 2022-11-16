1f7da004b87d97064e29686e9b7d033648f74175
JennAnd <89971271+JennAnd@users.noreply.github.com>
2022-11-17 00:40:10 +0100

    animation landingpage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,16 @@
 @extends('layout')
 @section('content')
 
+</div>
 <div class="landingpage-container">
     <img class="landingpage-image" src="/assets/landingpage.webp" />
 
-    <h1 class="landingpage-logo">LOGGA</h1>
+
 
 
     <div class="landingpage-text">
         <h1 class="landing-page-heading">Welcome to Ananda Yoga </h1>
-        <p class="landing-page-under-heading">lorem ipsum dolor sijjjjjjjjjjjjjjjjjt amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet</p>
+        <p class="landing-page-under-heading">L orem ipsum orem ipsum orem ipsum orem ipsum orem ipsum orem ipsum orem ipsum orem ipsum orem ipsum orem ipsum orem ipsum </p>
         <button class="landingpage-button">Become a member</button>
     </div>
 </div>
