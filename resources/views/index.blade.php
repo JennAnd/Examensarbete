a5e745b5b51069ee7c2d81604b44f88478c6b69b
JennAnd <89971271+JennAnd@users.noreply.github.com>
2022-11-17 01:31:16 +0100

    index finished

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,13 +5,10 @@
 <div class="landingpage-container">
     <img class="landingpage-image" src="/assets/landingpage.webp" />
 
-
-
-
     <div class="landingpage-text">
         <h1 class="landing-page-heading">Welcome to Ananda Yoga </h1>
-        <p class="landing-page-under-heading">L orem ipsum orem ipsum orem ipsum orem ipsum orem ipsum orem ipsum orem ipsum orem ipsum orem ipsum orem ipsum orem ipsum </p>
-        <button class="landingpage-button">Become a member</button>
+        <p class="landing-page-under-heading">We at Ananda Yoga offer yoga and meditation every day in our lovely shala in Bali. The classes are both for those who are beginners, as well as those who are experienced yogis and want to develop their practice. If you want to join us, please click the button below. </p>
+        <a href="/signup" class="landingpage-button"> Become a member</a>
     </div>
 </div>
 @endsection
