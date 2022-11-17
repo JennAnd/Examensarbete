339324a6b4c7778fba96a74acc2e414bd4b6da2b
JennAnd <89971271+JennAnd@users.noreply.github.com>
2022-11-17 15:55:58 +0100

    admin panel nav

--- resources/views/sidenav.blade.php
+++ resources/views/sidenav.blade.php
@@ -1,6 +1,11 @@
+@php use App\Models\User; @endphp
 <link rel="stylesheet" href="../stylesheets/sidenav.css">
 <div class="side-nav">
     <ul>
+        @if (Auth::user()->is_admin)
+        <li><a href="{{route('adminpanel')}}">Admin panel</a></li>
+        @endif
+
         <li><a href="{{route('dashboard')}}" class="{{ (request()->is('dashboard')) ? 'active-profile' : '' }}">My yogaclasses</a></li>
         <li><a href="{{route('profile')}}" class="{{ (request()->is('profile')) ? 'active-profile' : '' }}">My profile</a></li>
         <li><a href="{{route('payments')}}" class="{{ (request()->is('payments')) ? 'active-profile' : '' }}">Payments</a></li>
